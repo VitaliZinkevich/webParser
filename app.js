9bd06baec28a0f3eb338cf673dcace4da871f4ae
Vitalizinkevich <vitalizinkevich@gmail.com>
2018-05-17 17:34:12 +0300

    first level parse

--- app.js
+++ app.js
@@ -1,179 +1,296 @@
 'use strict'
 
-const scrape = require('website-scraper');//del
+let linksToParse = [
+'http://www.sanatorii.by/?Alesya',
+'http://www.sanatorii.by/?Belaya_vezha',
+/*'http://www.sanatorii.by/?Berestie',
+'http://www.sanatorii.by/?Bug',
+'http://www.sanatorii.by/?Kolos',
+'http://www.sanatorii.by/?Magistralny',
+'http://www.sanatorii.by/?Nadzeya',
+'http://www.sanatorii.by/?Ruzhanski',
+'http://www.sanatorii.by/?Svitanak_Brest',
+'http://www.sanatorii.by/?Svitanok_Brest',
+'http://www.sanatorii.by/?Solnechny',
+'http://www.sanatorii.by/?Chabarok',
+'http://www.sanatorii.by/?Energia',
+'http://www.sanatorii.by/?Yaselda',
+'http://www.sanatorii.by/?Borovoe',
+'http://www.sanatorii.by/?Vetraz',
+'http://www.sanatorii.by/?Zheleznodorozhnik',
+'http://www.sanatorii.by/?Jeleznyaki',
+'http://www.sanatorii.by/?Jemchujina_Vitebsk',
+'http://www.sanatorii.by/?Lepelski',
+'http://www.sanatorii.by/?Lesnoe',
+'http://www.sanatorii.by/?Lesnie_Ozera',
+'http://www.sanatorii.by/?Letcy',
+'http://www.sanatorii.by/?Naftan',
+'http://www.sanatorii.by/?Plissa',
+'http://www.sanatorii.by/?Rosinka',
+'http://www.sanatorii.by/?Sosnovi_Bor_Vitebsk',
+'http://www.sanatorii.by/?Vasilek',
+'http://www.sanatorii.by/?Gomelskogo_otdelenie_BJD',
+'http://www.sanatorii.by/?Zolotie_peski',
+'http://www.sanatorii.by/?Mashinostroitel',
+'http://www.sanatorii.by/?Pridneprovsky',
+'http://www.sanatorii.by/?Serebryanie_Klyuchi',
+'http://www.sanatorii.by/?Sidelniki',
+'http://www.sanatorii.by/?Solnechny_Bereg',
+'http://www.sanatorii.by/?Sosny_Gomel',
+'http://www.sanatorii.by/?Chonki',
+'http://www.sanatorii.by/?Alfa_Radon',
+'http://www.sanatorii.by/?Jemchujina_Grodno',
+'http://www.sanatorii.by/?Neman_72',
+'http://www.sanatorii.by/?Ozerny',
+'http://www.sanatorii.by/?Porechie',
+'http://www.sanatorii.by/?Praleska_Grodno',
+'http://www.sanatorii.by/?Radon',
+'http://www.sanatorii.by/?Raduga_Grodno',
+'http://www.sanatorii.by/?Svitiaz',
+'http://www.sanatorii.by/?Energetik',
+'http://www.sanatorii.by/?BATE',
+'http://www.sanatorii.by/?Belaya_Rus',
+'http://www.sanatorii.by/?Belino',
+'http://www.sanatorii.by/?Belorusochka',
+'http://www.sanatorii.by/?Berezina',
+'http://www.sanatorii.by/?Berezina_Borisov',
+'http://www.sanatorii.by/?Berezka',
+'http://www.sanatorii.by/?Vesta',
+'http://www.sanatorii.by/?Volma',
+'http://www.sanatorii.by/?Viaguti',
+'http://www.sanatorii.by/?Gorodische',
+'http://www.sanatorii.by/?Dudinka',
+'http://www.sanatorii.by/?Jdanovichy',
+'http://www.sanatorii.by/?Juravushka',
+'http://www.sanatorii.by/?Zeleni_Bor',
+'http://www.sanatorii.by/?Zeleni_bor_RC',
+'http://www.sanatorii.by/?Isloch',
+'http://www.sanatorii.by/?Krinitsa',
+'http://www.sanatorii.by/?Lazurny',
+'http://www.sanatorii.by/?Nadezhda',
+'http://www.sanatorii.by/?Narochanka',
+'http://www.sanatorii.by/?Narochanski_Bereg',
+'http://www.sanatorii.by/?Naroch',
+'http://www.sanatorii.by/?Ostroshickij_gorodok',
+'http://www.sanatorii.by/?Podjelniki',
+'http://www.sanatorii.by/?Praleska_Minsk',
+'http://www.sanatorii.by/?Primorski',
+'http://www.sanatorii.by/?Priozerny',
+'http://www.sanatorii.by/?Priozerny',
+'http://www.sanatorii.by/?Raketa',
+'http://www.sanatorii.by/?Rassvet_Luban',
+'http://www.sanatorii.by/?The_Republican_Hospital_of_Speleo_treatment',
+'http://www.sanatorii.by/?Rudnya',
+'http://www.sanatorii.by/?Svitanok_Minsk',
+'http://www.sanatorii.by/?Solnyshko',
+'http://www.sanatorii.by/?Sosnovi_Bor_Minsk',
+'http://www.sanatorii.by/?Sosny_Minsk',
+'http://www.sanatorii.by/?Sputnik',
+'http://www.sanatorii.by/?Talka',
+'http://www.sanatorii.by/?Balneolecheniya',
+'http://www.sanatorii.by/?Energetik_Minsk',
+'http://www.sanatorii.by/?Yunost',
+'http://www.sanatorii.by/?Dubrovenka',
+'http://www.sanatorii.by/?Lenina',
+'http://www.sanatorii.by/?Orlovskogo',
+'http://www.sanatorii.by/?Sosny_Mogilev',
+'http://www.sanatorii.by/?Chaika',
+'http://www.sanatorii.by/?Shinnik',
+'http://www.sanatorii.by/?Energetik_Mogilev'*/
+]
+
+
+console.log (linksToParse.length)
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+
 const cheerio = require('cheerio');
-const utf8 = require('utf8');//del
-var encoding = require("encoding");//del
-var xmaccyrillic = require('x-mac-cyrillic');//del
-
-var Buffer = require('buffer').Buffer;//del
-
-
-
-
-
-//const crawler = require("crawlerr");
-//const spider = crawler("http://www.sanatorii.by/");
-
 
 var request = require('request');
 var iconv = require('iconv');
 
-request.get({
-  //url: 'http://www.sanatorii.by/?Alesya',
-  url:'http://www.sanatorii.by/?Belaya_vezha',
-  encoding: null,
-}, function(err, res, body) {
 
-  var ic = new iconv.Iconv('CP1251', 'utf-8');
-  var buf = ic.convert(body);
-  var utf8String = buf.toString('utf-8');
-  const $ = cheerio.load(utf8String,  { decodeEntities: false })
-
-  let rooms = []
+// while for strings url at array, splice for reduce
 
 
-  class roomDescription {
+while ( linksToParse.length != 0 ) {
 
-  constructor(roomName, roomAccamodation, roomDetails) {
+let currentLink = linksToParse.splice (0,1)
+ currentLink = currentLink.join()
+ console.log (currentLink)
+ console.log (typeof currentLink)
+startParse (currentLink)
 
-    this.roomName = roomName
-    this.roomAccamodation = roomAccamodation
-    this.roomDetails = roomDetails
-
-  }
 
 }
 
+function startParse (currentLink) {
+
+  request.get({
+    url: currentLink,
+    //url: "http://www.sanatorii.by/?Alesya",
+    //url:'http://www.sanatorii.by/?Belaya_vezha',
+    encoding: null,
+  }, function(err, res, body) {
+
+    var ic = new iconv.Iconv('CP1251', 'utf-8');
+    var buf = ic.convert(body);
+    var utf8String = buf.toString('utf-8');
+    const $ = cheerio.load(utf8String,  { decodeEntities: false })
+
+    let currentObj = {}
+
+
+    let rooms = []
 
 
 
-  // размещение и название номера
-  let costTable = $('#costtable').find('.tr_hover.acceptor').find('.shadow').toArray()
-  for (var i = 0; i < costTable.length; i++) {
-    let roomAccamodation = []
-    let roomDetails = null
-    let roomName = null
+    class roomDescription {
 
-    costTable[i].children.forEach((elem, index)=>{
-      if (elem.hasOwnProperty ('attribs')) {
-        if (elem.attribs.title == undefined) {
-          roomDetails = elem.children[0].data
-          console.log (elem.children[0].data) // room details
-        } else {
-          roomAccamodation.push (elem.attribs.title)
-          console.log (elem.attribs.title) // room accomodation strings
+    constructor(roomName, roomAccamodation, roomDetails) {
+
+      this.roomName = roomName
+      this.roomAccamodation = roomAccamodation
+      this.roomDetails = roomDetails
+
+    }
+
+  }
+
+
+    // название и местонахождения объекта
+
+  let objectName = $('.obj_name').find('h1').text()
+  let objectObl = $('.obj_name').find('.box_obl').text()
+  currentObj.name = objectName
+  currentObj.region = objectObl
+  console.log (currentObj)
+
+
+
+  /*
+    // размещение и название номера
+    let costTable = $('#costtable').find('.tr_hover.acceptor').find('.shadow').toArray()
+    for (var i = 0; i < costTable.length; i++) {
+      let roomAccamodation = []
+      let roomDetails = null
+      let roomName = null
+
+      costTable[i].children.forEach((elem, index)=>{
+        if (elem.hasOwnProperty ('attribs')) {
+          if (elem.attribs.title == undefined) {
+            roomDetails = elem.children[0].data
+            //console.log (elem.children[0].data) // room details
+          } else {
+            roomAccamodation.push (elem.attribs.title)
+            //console.log (elem.attribs.title) // room accomodation strings
+          }
+
+
+
+        }
+
+        if (elem.hasOwnProperty ('data')) {
+          //console.log (elem.data) // room name
+          roomName = elem.data
         }
 
 
 
-      }
-
-      if (elem.hasOwnProperty ('data')) {
-        console.log (elem.data) // room name
-        roomName = elem.data
-      }
-
-
-
-    })
-    //console.log (rooms.length)
-    let currentRoom= new roomDescription(roomName, roomAccamodation, roomDetails)
-    rooms.push (currentRoom)
-  }
-
-  console.log (rooms)
-
-// цены по датам и вариантам размещения
-/*
-let priceTables = $('.newtab_noborder').toArray()
-//console.log (priceTables.length)
-
-priceTables.forEach ((elem1, index1)=>{
-
-
-elem1.children.forEach ((elem2 , index2)=>{ // tr
-  if (elem2.children == undefined) {
-
-
-  } else {
-
-    elem2.children.forEach ((elem3 , index3)=>{ // td
-
-
-if (elem3.children != undefined) {
-  elem3.children.forEach ((elemnt4, index4)=>{
-
-if (elemnt4.children != undefined) {
-  if (elemnt4.hasOwnProperty ('attribs')) {
-
-    if (elemnt4.attribs.title == undefined) {
-
-    } else {
-      console.log ("elemnt4.attribs.title")
-      console.log (elemnt4.attribs.title)
-
-
-    }
-  }
-
-  if (elemnt4.hasOwnProperty ('data')) {
-      console.log (elemnt4.data)
-      console.log ("elemnt4.data") //
-  }
-
-  elemnt4.children.forEach ((elemnt5, index5)=>{
-    if (elemnt5.hasOwnProperty ('attribs')) {
-
-      if (elemnt5.attribs.title == undefined) {
-        //console.log (elemnt4) //
-      } else {
-        console.log (elemnt5.attribs.title)
-          console.log ('elemnt5.attribs.title') //
-      }
+      })
+      //console.log (rooms.length)
+      let currentRoom= new roomDescription(roomName, roomAccamodation, roomDetails)
+      rooms.push (currentRoom)
     }
 
-    if (elemnt5.hasOwnProperty ('data')) {
 
-      if (elemnt5.data == '') {
+    currentObj.rooms= rooms
+  */
+  /*
+  // цены по датам и вариантам размещения
+  let roomSeparator = '';
+  let roomSeparatorStatus = false;
 
-      } else {
+  let priceTableAsString = ''
+  let index = 0;
 
-        console.log (elemnt5.data)
-        console.log ('elemnt5.data')
+  let priceTables = $('.newtab_noborder').find('td').each (function(i, elem) {
 
-       if (elemnt5.data.indexOf('16-Мая') != -1){
-          console.log ('new')
 
-        }
-      }
+    if ($(this).text() == roomSeparator) {
 
+      console.log ('-------------------------------------');
+
+       currentObj.rooms[index].priceTableAsString = priceTableAsString
+       priceTableAsString = ''
+       index++
     }
 
-  })
+  priceTableAsString += $(this).text()
 
-}
+  if (index == currentObj.rooms.length-1) {
+  currentObj.rooms[index].priceTableAsString = priceTableAsString
+
+  }
+
+  if (!roomSeparatorStatus) {
+    $(this).text()
+    //console.log ($(this).text().length)
+    roomSeparator = $(this).text()
+    //console.log(roomSeparator)
+    roomSeparatorStatus = true
+
+  }
 
 
   })
+  */
+
+  // статическая информация по объекту
+  /*
+  let textInfo = ''
+  $('#staticContent').find ('p').each (function (i, e) {
+    console.log ($(this).text()) // текст из абзацев в статическом контенте
+    textInfo +=$(this).text()
+  })
+
+
+  let listInfo = ''
+  $('#staticContent').find ('li').each (function (i, e){
+  console.log ($(this).text()) // списки, общие описание
+  listInfo += $(this).text()
+  })
+
+  let linkInfo= ''
+  $('#staticContent').find ('a').each (function (i, e) {
+    console.log ($(this).attr('href'))
+  linkInfo+=$(this).attr('href')
+     // линки на фото
+  })
+
+
+  currentObj.textInfo = textInfo
+  currentObj.lists = listInfo
+  currentObj.links = linkInfo
+  */
+
+
+
+
+
+
+  MongoClient.connect('mongodb://localhost:27017/sanatoriiby', { useNewUrlParser: true }, function(err, result) {
+    assert.equal(null, err);
+    const db = result.db('sanatoriiby');
+    db.collection('sanatorium').insert (currentObj)
+    currentObj = {} // clean after iteration
+
+  });
+
+
+
+  });
+
 }
-
-
-    })
-
-
-  }
-
-
-})
-
-})*/
-
-
-
-
-
-
-
-
-});
